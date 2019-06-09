2f0eee422cb1a510405193048eb941ac7ed6d2b4
dzordich <dzordich@gmail.com>
2019-06-08 21:12:02 -0400

    fixed a ton of bugs. added functions that more accurately compares lists of words with a certain letter in EACH index, not just the first instance of the letter. PLEASE NOTE the program has a massive amount of lag in the first round as it is looping through each word in the list multiple times. I would recommend testing it on easy mode first as easy mode has the smallest initial list of words (5272 vs hard mode's 29989). I will be working on reducing the lag tomorrow.

--- demon_words.py
+++ demon_words.py
@@ -7,7 +7,7 @@ from random import random
 
 #prompts user to select difficulty and returns that difficulty
 def user_choose_diff():
-    diff_choice = input("Please choose a difficulty: 1. Easy, 2. Normal, 3. Hard ")
+    diff_choice = input("Please choose a difficulty: 1. Easy (4 letter words), 2. Normal (6 letter words), 3. Hard(8 letter words) ")
     if diff_choice == "1" or diff_choice.lower() == "normal":
         diff = 1 
         print("You've selected easy difficulty.")
@@ -33,16 +33,17 @@ def game(word_list):
     round = 8
     diff = 2
     guesses = []
+    incorrect_guesses = []
     valid_guess = "abcdefghijklmnopqrstuvwxyz"
 
     #sets list of words for each diff
     for word in word_list:
-        if len(word) > 4 and len(word) < 6:
-            ez_words.append(word)
-        elif len(word) >= 6 and len(word) <= 8:
-            norm_words.append(word)
-        elif len(word) >= 9:
-            hard_words.append(word)
+        if len(word.strip()) == 4:
+            ez_words.append(word.lower().strip())
+        elif len(word.strip()) == 6:
+            norm_words.append(word.lower().strip())
+        elif len(word.strip()) == 8:
+            hard_words.append(word.lower().strip())
 
     #gets user's choice of diff
     diff = user_choose_diff()
@@ -50,23 +51,25 @@ def game(word_list):
     #sets the initial list of words that will be used for the game, based on diff
     if diff == 1:
         active_word_list = ez_words
+        length_of_word = 4
     elif diff == 2:
         active_word_list = norm_words
+        length_of_word = 6
     elif diff == 3:
         active_word_list = hard_words
+        length_of_word = 8
 
     #sets word and lists for game
     #in this version of the game the actual value of word does not matter. However,
     #I left it in because length_of_word is still used.
     showListSize = input("Would you like to see the size of the word list while you are playing? y/n ")
-    word = wordPicker(active_word_list)
-    word = word.strip().lower()
-    length_of_word = len(word)
     answer_reveal = ""
     outcome = ""
     hidden_word = []
-    for i in word:
+    i = 0
+    while i < length_of_word:
         hidden_word.append("_")
+        i += 1
     print(" ")
     print("Get ready. The word is ", length_of_word, " letters long.")
     print(" ")
@@ -83,37 +86,35 @@ def game(word_list):
         print(" ")
         round_guess = input("Please guess a letter: ").lower()
         #active_word_list is reset at the point in each round where the user makes a guess
-        active_word_list = evilList(active_word_list, round_guess, length_of_word)
-        if round_guess not in valid_guess or len(round_guess) > 1:
+        active_word_list = evilList(active_word_list, round_guess, length_of_word, incorrect_guesses)
+        if round_guess not in valid_guess or len(round_guess) != 1:
             print("Not a valid guess. ")
         elif round_guess in guesses:
             print("You've already guessed that letter. Try a different one. ")
+        elif guess_is_right(round_guess, active_word_list):
+            index = list_of_indexes(round_guess, active_word_list[0])
+            for i in index:
+                hidden_word[i] = round_guess
+            
+            print(*hidden_word)
+            print("Correct guess. ")
+            if "_" not in hidden_word:
+                print("You won! The word was " + (answer_reveal.join(hidden_word)).upper())
+                outcome = "w"
+                round = 0
+            else:
+                print("You have " + str(round - 1) + " rounds remaining. ")
         else:
-            for i in active_word_list:
-                if round_guess in i:
-                    index_to_reveal = i.find(round_guess)
-                    hidden_word[index_to_reveal] = round_guess
-                    print(*hidden_word)
-                    print("Correct guess. ")
-                    if "_" not in hidden_word:
-                        print("You won! The word was " + (answer_reveal.join(hidden_word)).upper())
-                        outcome = "w"
-                        round = 0
-                        break
-                    else:
-                        print("You have " + str(round - 1) + " rounds remaining. ")
-                        break
-                else:
-                    print(*hidden_word)
-                    print("Incorrect guess. ")
-                    print("You have " + str(round - 1) + " rounds remaining. ")
-                    break
-            round = round -1
-            guesses.append(round_guess)
+            print(*hidden_word)
+            print("Incorrect guess. ")
+            incorrect_guesses.append(round_guess)
+            print("You have " + str(round - 1) + " rounds remaining. ")
+        round = round -1
+        guesses.append(round_guess)
         
     if outcome != "w":
         #picks a random word from the active list to display
-        word = wordPicker(active_word_list)
+        word = wordPicker(active_word_list, hidden_word)
         print("You lost! You ran out of guesses. The word was " + word.upper())
 
     playAgain(input("Would you like to play again? y/n: "))
@@ -134,13 +135,27 @@ def playAgain (u):
 #then checks if the word families containing the guess in each index is larger than the base case.
 #resets the base case if the new family is larger.
 #returns the longest list.
-def evilList(words, guess, word_length):
+def evilList(words, guess, word_length, guesses):
     longest_list = []
+    
     for w in words:
-        if guess not in w:
+        if guess not in w and guessed_letter_in_word(w, guesses) == 0 :
             longest_list.append(w)
-    i = 0
-    while i <= word_length:
+    # print(*longest_list, "***INITIAL***")
+    for w in words:
+        if len(words_w_same_index(words, w, guess)) > len(longest_list):
+            longest_list = words_w_same_index(words, w, guess)
+            # print(*longest_list, "***NEW***")
+    return longest_list
+
+def reveal(words, guess, word_length):
+    longest_list = []
+    
+    for w in words:
+        if w[0] == guess:
+            longest_list.append(w)
+    i = 1
+    while i < word_length:
         list_to_compare = []
         for w in words:
             if w[i] == guess:
@@ -150,10 +165,49 @@ def evilList(words, guess, word_length):
         i += 1
     return longest_list
 
-def wordPicker(listWords):
-    word = listWords[int(random() * len(listWords))]
-    return word
+def wordPicker(listWords, hidden_word):
+    for word in listWords:
+        boolean = True
+        ind = 0
+        for i in hidden_word:
+            if i != word[ind] and i != "_":
+                boolean = False
+            ind += 1
+        if boolean:
+            return word
+            
 
+def guessed_letter_in_word(word, letters):
+    count = 0
+    for l in letters:
+        for i in word:
+            if l == i:
+                count += 1
+    return count
+
+def guess_is_right(guess, active_list):
+    for word in active_list:
+        if guess in word:
+            return True
+    return False
+
+#returns a list of indexes of guess in a word
+def list_of_indexes(guess, word):
+    index = []
+    x = 0
+    for i in word:
+        if i == guess:
+            index.append(x)
+        x += 1
+    return index
+
+def words_w_same_index(words, word, guess):
+    same_words = []
+    same_words.append(word)
+    for w in words:
+        if list_of_indexes(guess, w) == list_of_indexes(guess, word):
+            same_words.append(w)
+    return same_words
 
 
 game(word_list)
